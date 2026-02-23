369d23edd798d7c255d706a70e419fb00476f1d2
Titus <tistauffer@ascensionacademy.org>
2026-02-23 08:55:44 -0600

    background sized

--- soundboard/ContentView.swift
+++ soundboard/ContentView.swift
@@ -19,6 +19,7 @@ struct ContentView: View {
         ZStack {
             Image(.backdrop)
                 .resizable()
+                .ignoresSafeArea()
             VStack {
                 Spacer()
                 Button("Sheep") {
